c74d6d1e287bcb197630f4d696868f3fe824126f
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-03-29 17:19:44 -0700

    koch finished

--- Koch.java
+++ Koch.java
@@ -8,6 +8,28 @@ public class Koch{
       Output:
          StdDraw: an order n Koch curve is drawn to StdDraw
       */
+      if (n == 0) {
+         t.step(length);   
+      }
+      else if (n == 1) {
+         t.step(length/3);
+         t.turn(Math.PI/3);
+         t.step(length/3);
+         t.turn(-2*Math.PI/3);
+         t.step(length/3);
+         t.turn(Math.PI/3);
+         t.step(length/3);
+         
+      }
+      else {
+         kochHelper(n-1, t, length/3);
+         t.turn(Math.PI/3);
+         kochHelper(n-1, t, length/3);
+         t.turn(-2*Math.PI/3); 
+         kochHelper(n-1, t, length/3);
+         t.turn(Math.PI/3);
+         kochHelper(n-1, t, length/3);
+      }
    }
    public static void kochCurve(int n){
       StdDraw.setXscale(0, 1);
